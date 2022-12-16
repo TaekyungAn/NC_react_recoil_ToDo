1f97afa31b947168458191ddbfffd2e86e33c490
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-12-16 16:58:12 +0900

    4.2~4.3 createBrowserRouter and errorElement'

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,4 +1,4 @@
 function About() {
-  return <>About</>;
+  return <h1>About</h1>;
 }
 export default About;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,5 @@
 function Home() {
-  return <>Home</>;
+  const users: any = [];
+  return <h1>{users[0].name}</h1>;
 }
 export default Home;
