9bc4b166a896b5b59ad832063ffdfcbdfc23c95d
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2022-12-22 21:30:11 +0900

    5.9 React Query part One

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
@@ -5,11 +6,15 @@ import { ThemeProvider } from "styled-components";
 import router from "./Router";
 import { theme } from "./theme";
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <ThemeProvider theme={theme}>
-    <RouterProvider router={router} />
-  </ThemeProvider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </QueryClientProvider>
 );
