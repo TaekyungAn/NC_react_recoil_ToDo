75f3ebbc16cc591dbac243d594b4256af3dc86d2
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-02 22:49:48 +0900

    6.7 Form validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -25,17 +25,46 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
 
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("FirstName")} placeholder="FirstName" />
-        <input {...register("LastName")} placeholder="LastName" />
-        <input {...register("Password")} placeholder="Password" />
-        <input {...register("Password1")} placeholder="Password1" />
+      <form
+        style={{ display: "flex", flexDirection: "column" }}
+        onSubmit={handleSubmit(onValid)}
+      >
+        {/* 자바스크립트에서 validation(required)하면 html에서 해주는 것보다 보안이 잘 됨 */}
+        <input {...register("Email", { required: true })} placeholder="Email" />
+        <input
+          {...register("FirstName", { required: true })}
+          placeholder="FirstName"
+        />
+        <input
+          {...register("LastName", { required: true })}
+          placeholder="LastName"
+        />
+        <input
+          {...register("username", { required: true, minLength: 10 })}
+          placeholder="Username"
+        />
+        <input
+          {...register("Password", { required: true, minLength: 5 })}
+          placeholder="Password"
+        />
+        <input
+          {...register("Password1", {
+            required: "Password is required",
+            minLength: {
+              value: 5,
+              message: "Your password is too short.",
+            },
+          })}
+          placeholder="Password1"
+        />
         <button>Add</button>
       </form>
     </div>
