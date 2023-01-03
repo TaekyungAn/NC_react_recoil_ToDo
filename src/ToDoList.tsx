8b5e91cf66af09c1b7d8766a02e3c067c2cc2b08
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-03 22:15:08 +0900

    6.10 Recap

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,126 +1,27 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-
-// function ToDoList() {
-//   const [toDo, setTodo] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     //**es6문법 */
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     setTodo(value);
-//   };
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(toDo);
-//   };
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} placeholder="Write a to do" value={toDo} />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
-type IForm = {
-  username: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
-  password1: string;
-  extraError?: StringConstructor;
-};
+interface IFrom {
+  toDo: string;
+}
 function ToDoList() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors },
-    setError,
-  } = useForm<IForm>({ defaultValues: { email: "@naver.com" } });
-
-  const onValid = (data: IForm) => {
-    if (data.password !== data.password1) {
-      setError(
-        "password1",
-        { message: "Password are not the same" },
-        { shouldFocus: true }
-      );
-    }
-    // setError("extraError", { message: "Server offline" });
+  } = useForm<IFrom>();
+  const onSubmit = (data: IFrom) => {
+    console.log("add to do ", data.toDo);
+    setValue("toDo", "");
   };
-
-  console.log(errors);
-
   return (
     <div>
-      <form
-        style={{ display: "flex", flexDirection: "column" }}
-        onSubmit={handleSubmit(onValid)}
-      >
-        {/* 자바스크립트에서 validation(required)하면 html에서 해주는 것보다 보안이 잘 됨 */}
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
-          {...register("email", {
-            required: "email is reqired",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com emails allowed",
-            },
-          })}
-          placeholder="email"
+          {...register("toDo", { required: "Please write to do" })}
+          placeholder="Write a to do"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("firstName", {
-            required: "Write here",
-            validate: {
-              noNico: (value) =>
-                value.includes("nico") ? "no nicos allowed" : true,
-              noNick: (value) =>
-                value.includes("nick") ? "no nicks allowed" : true,
-            },
-          })}
-          placeholder="firstName"
-        />
-        <span>{errors?.firstName?.message}</span>
-        <input
-          {...register("lastName", { required: "Write here" })}
-          placeholder="lastName"
-        />
-        <span>{errors?.lastName?.message}</span>
-        <input
-          {...register("username", {
-            required: "Write here",
-            minLength: { value: 5, message: "more than 5 letters" },
-          })}
-          placeholder="username"
-        />
-        <span>{errors?.username?.message}</span>
-        <input
-          {...register("password", {
-            required: "Write here",
-            minLength: {
-              value: 5,
-              message: "Your password is too short.",
-            },
-          })}
-          placeholder="password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password1", {
-            required: "Password is required",
-            minLength: {
-              value: 5,
-              message: "Your password is too short.",
-            },
-          })}
-          placeholder="password1"
-        />
-        <span>{errors?.password1?.message}</span>
+        <span>{errors.toDo?.message}</span>
         <button>Add</button>
-        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
