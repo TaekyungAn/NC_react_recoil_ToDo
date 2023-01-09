b120c3e1a46eb361fedb43a39b7cbd349be94ffb
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-10 00:13:43 +0900

    6.13 Categories(방법1)

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,21 @@
 import { IToDo } from "./atoms";
 
 function ToDo({ text, category }: IToDo) {
+  const onClick = (newCategory: IToDo["category"]) => {
+    console.log(newCategory);
+  };
   return (
     <li>
       <span>{text}</span>
-      {category !== "TO_DO" && <button>To Do</button>}
-      {category !== "DOING" && <button>Doing</button>}
-      {category !== "DONE" && <button>Done</button>}
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
