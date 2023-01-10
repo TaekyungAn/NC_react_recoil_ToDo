40b19abafeb78be7592291e377e2ab2a7f360999
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-01-10 19:58:03 +0900

    6.16 Selector part One

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useRecoilValue } from "recoil";
-import { toDoState } from "./atoms";
+import { toDoSelector, toDoState } from "./atoms";
 import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
@@ -7,17 +7,35 @@ function ToDoList() {
   // vlaue와 변경함수 둘 다 갖으려면 아래 useRecoilState사용
   // const [toDos, setToDos] = useRecoilState(toDoState);
   // 따로 사용 시 아래처럼 사용
-  const toDos = useRecoilValue(toDoState);
+  // const toDos = useRecoilValue(toDoState);
   // const modFn = useRecoilState(toDoState);
-  console.log(toDos);
+
+  // const selectorOutput = useRecoilValue(toDoSelector);
+  // 위 selectorOutput의 안에 있는 배열 꺼내는 방법 => 순서대로 이름 지정
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
 
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
