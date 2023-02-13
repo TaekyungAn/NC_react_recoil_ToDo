aa8a93fd144214a77f516c777c3bd66aa7c38875
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-02-13 21:37:23 +0900

    6.8 재업)Form Errors

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IFrom {
+  email: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  password: number;
+  password1: number;
+}
+
 function ToDoList() {
-  const { register, handleSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFrom>({
+    defaultValues: {
+      email: "@naver.com",
+    },
+  });
   // 데이터가 유효하지 않으면 error를 나타내고, 모든 validation을 마치면 호출
   const onValid = (data: any) => {
     console.log(data);
   };
-  console.log(formState.errors);
+  console.log(errors);
 
   return (
     <div>
@@ -15,18 +32,32 @@ function ToDoList() {
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("email", { required: true })} placeholder="Email" />
         <input
-          {...register("firstname", { required: true })}
+          {...register("email", {
+            required: "Email is required",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
+              message: "only naver.com emails allowed",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstname", { required: "First Name is required" })}
           placeholder="First Name"
         />
+        <span>{errors?.firstname?.message}</span>
+
         <input
-          {...register("lastname", { required: true })}
+          {...register("lastname", { required: "Last Name is required" })}
           placeholder="Last Name"
         />
+        <span>{errors?.lastname?.message}</span>
+
         <input
           {...register("username", {
-            required: true,
+            required: "Username is required",
             minLength: {
               value: 2,
               message: "Your name is too short",
@@ -34,14 +65,19 @@ function ToDoList() {
           })}
           placeholder="Username"
         />
+        <span>{errors?.username?.message}</span>
+
         <input
           {...register("password", { required: "password is required" })}
           placeholder="Password"
         />
+        <span>{errors?.password?.message}</span>
+
         <input
           {...register("password1", { required: "password is required" })}
           placeholder="Password1"
         />
+        <span>{errors?.password1?.message}</span>
 
         <button>Add</button>
       </form>
