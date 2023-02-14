e85dd676fca178c349afdaf09532ba42fa19376a
atk0426@naver.com <taekyung@DESKTOP-HRK1UQR.localdomain>
2023-02-14 23:20:01 +0900

    6.13 재업)Categories-1

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,21 @@
 import { IToDo } from "../atom";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category }: IToDo) {
+  const onClick = (newCategory: IToDo["category"]) => {
+    console.log("i wanna go to", newCategory);
+  };
   return (
     <li>
       {text}
-      <button>To Do</button>
-      <button>Doing</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button onClick={() => onClick("DOING")}>Doing</button>
+      )}
+      {category !== "TO_DO" && (
+        <button onClick={() => onClick("TO_DO")}>To Do</button>
+      )}
+      {category !== "DONE" && (
+        <button onClick={() => onClick("DONE")}>Done</button>
+      )}
     </li>
   );
 }
